a265017d6562c315f8cb1f3a6cbca46af08dc290
toewailin <toewailinbdl@gmail.com>
2025-02-07 04:33:16 +0630

    add dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,287 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100" data-bs-theme="auto">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
+        <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
+
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@yield('title', 'Laravel')</title>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!--     Fonts and icons     -->
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
+        <!-- Nucleo Icons -->
+        <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+        <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+        <!-- Font Awesome Icons -->
+        <!-- Font Awesome CDN -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Material Icons -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+        <!-- CSS Files -->
+        <link id="pagestyle" href="{{ asset('assets/css/material-dashboard.css?v=3.2.0') }}" rel="stylesheet" />
+        @stack('styles')
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <body class="g-sidenav-show  bg-gray-100">
+        @include('layouts.partials.sidebar')
+        <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+            @yield('content')
+        </main>
+        @include('layouts.partials.footer')
+        <!--   Core JS Files   -->
+        <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins/chartjs.min.js') }}"></script>
+        <script>
+            var ctx = document.getElementById("chart-bars").getContext("2d");
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+            new Chart(ctx, {
+            type: "bar",
+            data: {
+                labels: ["M", "T", "W", "T", "F", "S", "S"],
+                datasets: [{
+                label: "Views",
+                tension: 0.4,
+                borderWidth: 0,
+                borderRadius: 4,
+                borderSkipped: false,
+                backgroundColor: "#43A047",
+                data: [50, 45, 22, 28, 50, 60, 76],
+                barThickness: 'flex'
+                }, ],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                legend: {
+                    display: false,
+                }
+                },
+                interaction: {
+                intersect: false,
+                mode: 'index',
+                },
+                scales: {
+                y: {
+                    grid: {
+                    drawBorder: false,
+                    display: true,
+                    drawOnChartArea: true,
+                    drawTicks: false,
+                    borderDash: [5, 5],
+                    color: '#e5e5e5'
+                    },
+                    ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 500,
+                    beginAtZero: true,
+                    padding: 10,
+                    font: {
+                        size: 14,
+                        lineHeight: 2
+                    },
+                    color: "#737373"
+                    },
+                },
+                x: {
+                    grid: {
+                    drawBorder: false,
+                    display: false,
+                    drawOnChartArea: false,
+                    drawTicks: false,
+                    borderDash: [5, 5]
+                    },
+                    ticks: {
+                    display: true,
+                    color: '#737373',
+                    padding: 10,
+                    font: {
+                        size: 14,
+                        lineHeight: 2
+                    },
+                    }
+                },
+                },
+            },
+            });
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+
+            var ctx2 = document.getElementById("chart-line").getContext("2d");
+
+            new Chart(ctx2, {
+            type: "line",
+            data: {
+                labels: ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+                datasets: [{
+                label: "Sales",
+                tension: 0,
+                borderWidth: 2,
+                pointRadius: 3,
+                pointBackgroundColor: "#43A047",
+                pointBorderColor: "transparent",
+                borderColor: "#43A047",
+                backgroundColor: "transparent",
+                fill: true,
+                data: [120, 230, 130, 440, 250, 360, 270, 180, 90, 300, 310, 220],
+                maxBarThickness: 6
+
+                }],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                legend: {
+                    display: false,
+                },
+                tooltip: {
+                    callbacks: {
+                    title: function(context) {
+                        const fullMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+                        return fullMonths[context[0].dataIndex];
+                    }
+                    }
+                }
+                },
+                interaction: {
+                intersect: false,
+                mode: 'index',
+                },
+                scales: {
+                y: {
+                    grid: {
+                    drawBorder: false,
+                    display: true,
+                    drawOnChartArea: true,
+                    drawTicks: false,
+                    borderDash: [4, 4],
+                    color: '#e5e5e5'
+                    },
+                    ticks: {
+                    display: true,
+                    color: '#737373',
+                    padding: 10,
+                    font: {
+                        size: 12,
+                        lineHeight: 2
+                    },
+                    }
+                },
+                x: {
+                    grid: {
+                    drawBorder: false,
+                    display: false,
+                    drawOnChartArea: false,
+                    drawTicks: false,
+                    borderDash: [5, 5]
+                    },
+                    ticks: {
+                    display: true,
+                    color: '#737373',
+                    padding: 10,
+                    font: {
+                        size: 12,
+                        lineHeight: 2
+                    },
+                    }
+                },
+                },
+            },
+            });
+
+            var ctx3 = document.getElementById("chart-line-tasks").getContext("2d");
+
+            new Chart(ctx3, {
+            type: "line",
+            data: {
+                labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                datasets: [{
+                label: "Tasks",
+                tension: 0,
+                borderWidth: 2,
+                pointRadius: 3,
+                pointBackgroundColor: "#43A047",
+                pointBorderColor: "transparent",
+                borderColor: "#43A047",
+                backgroundColor: "transparent",
+                fill: true,
+                data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+                maxBarThickness: 6
+
+                }],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                legend: {
+                    display: false,
+                }
+                },
+                interaction: {
+                intersect: false,
+                mode: 'index',
+                },
+                scales: {
+                y: {
+                    grid: {
+                    drawBorder: false,
+                    display: true,
+                    drawOnChartArea: true,
+                    drawTicks: false,
+                    borderDash: [4, 4],
+                    color: '#e5e5e5'
+                    },
+                    ticks: {
+                    display: true,
+                    padding: 10,
+                    color: '#737373',
+                    font: {
+                        size: 14,
+                        lineHeight: 2
+                    },
+                    }
+                },
+                x: {
+                    grid: {
+                    drawBorder: false,
+                    display: false,
+                    drawOnChartArea: false,
+                    drawTicks: false,
+                    borderDash: [4, 4]
+                    },
+                    ticks: {
+                    display: true,
+                    color: '#737373',
+                    padding: 10,
+                    font: {
+                        size: 14,
+                        lineHeight: 2
+                    },
+                    }
+                },
+                },
+            },
+            });
+        </script>
+        <script>
+            var win = navigator.platform.indexOf('Win') > -1;
+            if (win && document.querySelector('#sidenav-scrollbar')) {
+            var options = {
+                damping: '0.5'
+            }
+            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+            }
+        </script>
+        <!-- Github buttons -->
+        <script async defer src="{{ asset('assets/js/buttons.js') }}"></script>
+        <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+        <script src="{{ asset('assets/js/material-dashboard.min.js?v=3.2.0') }}"></script>
+        @stack('scripts')
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('backend.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -17,4 +17,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
